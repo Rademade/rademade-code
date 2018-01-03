210d0bb51cc52cd7e88528a49b5d615ce857dd29
igo <ig@rademade.com>
2018-01-03 16:51:16 +0200

    add jenkins deploy script

---
+++ jenkins-deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -eux
+
+gem install bundler
+
+bundle install
+
+yarn install
+
+#build frontend
+bundle exec rake assets:precompile
+
+#build and push docker image
+sudo docker login --username=$REGISTRY_USER --password=$REGISTRY_PASS $REGISTRY
+
+sudo docker build -t $REGISTRY/$PROJECT_NAME:latest .
+
+sudo docker push $REGISTRY/$PROJECT_NAME:latest
+
+#update service on docker manager
+ssh  $DOCKER_USER@$DOCKER_HOST 'docker service update --image $REGISTRY/$PROJECT_NAME:latest rademade-code_app'
+
