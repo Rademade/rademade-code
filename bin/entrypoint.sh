2b6e3b0e7b41df1f370f8274c86e9303a8093089
igo <ig@rademade.com>
2018-01-05 11:17:33 +0200

    edit entrypoint for docker image

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,5 +3,7 @@
 rm -rf ./static/*
 cp -R ./public/* ./static/
 
+until nc -z $POSTGRES_HOST 5432; do echo Waiting for postgresql; sleep 1; done
+
 bundle exec rake db:migrate
 bundle exec puma -C config/puma.rb
