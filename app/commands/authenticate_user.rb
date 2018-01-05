fb48e1cfc725584776e852e2762a67e3df6e8d37
Maksym Shutiak <max@rademade.com>
2018-01-05 15:19:47 +0200

    Fix bug with user creation

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -7,7 +7,7 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id, email: user.email, is_admin: user.is_admin) if user
+    ::JsonWebToken.encode(user_id: user.id, email: user.email, is_admin: user.is_admin) if user
   end
 
   private
